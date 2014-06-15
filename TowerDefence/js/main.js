341747801b45ced9edc980dddac79fa2cbb6b9b9
silvina-avramova <silvina.avramova@gmail.com>
2014-06-15 20:46:01 +0300

    enemy render

--- TowerDefence/js/main.js
+++ TowerDefence/js/main.js
@@ -5,21 +5,22 @@
     var gameEngine = GameEngine.getInstance();
     gameEngine.run();
 
-    var width = 640;
+    //var width = 640;
     //stage = new Kinetic.Stage({
     //    width: width,
     //    height: width,
     //    container: 'game-frame'
     //});
+    //layer = new Kinetic.Layer();
 
-    var gameField = GameField.getInstance(0, 0, width, width, null);
+    //var gameField = GameField.getInstance(0, 0, width, width, null);
 
-    var initialPoint = gameField.getPath[0];
-    if (initialPoint === undefined) {
-        initialPoint = { x: 0, y: 0 }
-    }
-    var enemy = Enemy.getInstance(initialPoint.x*32, initialPoint.y*32);
-    enemy.render(gameEngine.layer);
+    //var initialPoint = gameField.getPath[0];
+    //if (initialPoint === undefined) {
+    //    initialPoint = { x: 0, y: 0 }
+    //}
+    //var enemy = Enemy.getInstance(initialPoint.x*32, initialPoint.y*32);
+    //enemy.render(layer);
     //stage.add(layer);
 
 })();
