136563a239fd85c5f4d74398064a00200f7f77df
silvina-avramova <silvina.avramova@gmail.com>
2014-06-15 18:56:18 +0300

    enemy.js render method added

--- TowerDefence/js/enemy.js
+++ TowerDefence/js/enemy.js
@@ -1,4 +1,5 @@
 ﻿/// <reference path="minion.js" />
+/// <reference path="vendor/dejavu.js" />
 'use strict';
 
 var Enemy = dejavu.Class.declare({
@@ -8,9 +9,22 @@ var Enemy = dejavu.Class.declare({
         SPEED: 5,
         HEALTH_POINTS: 100,
         REWARD: 300,
+		SIZE: 32
     },
 
     initialize: function (x, y) {
         this.$super(x, y, this.$static.SPEED, this.$static.HEALTH_POINTS, this.$static.REWARD);
-    }
+    },
+	
+    render: function (layer) {
+    circle = new Kinetic.Circle({
+        x: this.getX,
+        y: this.getY,
+        radius: 0 | (this.$static.SIZE / 2),
+        fill: rgb(130, 181, 23),
+        stroke: 'none'
+    });
+    layer.add(circle);
+}
+	
 });
